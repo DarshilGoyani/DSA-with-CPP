ec8cc1633045fd49d735bc0dedb0b3611ad702c7
DarshilGoyani <darshilgoyani05@gmail.com>
2025-06-27 10:47:58 +0530

    adding all programm

---
+++ chapter-1/1-3/q-1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+main(){
+    cout << "Name \t : Darshil Goyani" <<endl;
+    cout << "Age \t : 19" <<endl;
+    cout << "School \t : Ashadeep IIT" <<endl;
+    
+}
---
+++ chapter-2/2-2/circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+main(){
+    float r;
+    cout << "Enter The Radius of Circle\t:" ; 
+    cin >> r;
+    cout << "Area of Circle :=\t" << PI*r*r;
+}
