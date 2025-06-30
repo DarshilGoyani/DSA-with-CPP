6bf8ba688149711cf9edfcc6362d359d83f5f112
DarshilGoyani <darshilgoyani05@gmail.com>
2025-06-30 14:37:17 +0530

    Ch-3

---
+++ chapter-3/3-1/unary.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+    int n=25;
+    // cout << n++ << --n << ++n << n--; 
+    cout << n++ <<endl;
+    cout << --n <<endl;
+    cout << ++n <<endl;
+    cout << n-- <<endl;
+} 
---
+++ chapter-3/3-2/q-5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+    // (x+y)^3
+    float x,y;
+    cout << "Enter the Value of X := ";
+    cin >> x;
+    cout << endl <<"Enter the Value of Y := ";
+    cin >> y;
+    cout << endl << "(x+y)^3 = " << (x+y)*(x+y)*(x+y);
+
+
+}
