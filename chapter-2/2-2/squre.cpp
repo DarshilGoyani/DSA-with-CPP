b6ddbe3c2612ca8a70682b4d27c15440961cd117
DarshilGoyani <darshilgoyani05@gmail.com>
2025-07-04 11:12:48 +0530

    Delete chapter-2 directory

--- chapter-2/2-2/squre.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-main(){
-    float l;
-    cout << "Enter the Length of one Side :=\t";
-    cin >> l;
-    cout << endl <<endl <<endl;
-    cout << "Area of Square :=\t" << l*l;
-}
